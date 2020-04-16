20b7a0b53b99038f74a6a3fac7dd1e6569d7c526
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-15 22:20:58 -0500

    change scope, reorganize script

--- DataExploration/dataExploration.py
+++ DataExploration/dataExploration.py
@@ -1,9 +1,13 @@
+#CS4331 Jacob Zahn
+import sys
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import stats
 
+sys.path.append('..')
 import lib
 
 CCH = lib.loadCCHTimeSeries()
@@ -15,38 +19,48 @@ nonProf = CCH['nonProf'].to_numpy()
 private = CCH['private'].to_numpy()
 governm = CCH['governm'].to_numpy()
 totalHosp = nonProf + private + governm
-casesLast= CCH['c2020-04-12'].to_numpy()
+casesLast = CCH['c2020-04-12'].to_numpy()
+deathsLast = CCH['d2020-04-12'].to_numpy()
 
 bedsZ = stats.zscore(beds)
 populationZ = stats.zscore(population)
 
-plt.figure()
-plt.scatter(population,casesLast)
-plt.title('casesLast vs population')
-plt.xlabel('County population')
-plt.ylabel('County cases')
-
-plt.figure()
-plt.scatter(beds,casesLast)
-plt.title('casesLast vs beds')
-plt.xlabel('County hospital beds')
-plt.ylabel('County cases')
-
+print('Explore multivariate relationships among the variables')
 plt.figure()
 plt.scatter(population,beds, c=casesLast, cmap=matplotlib.cm.cool)
 plt.title('beds vs population, colored by cases')
 plt.xlabel('County population')
 plt.ylabel('County hospital beds')
 
-#clean up scatter to remove non zero elements
-nonZeroMap = casesLast!=0
+plt.figure()
+plt.scatter(populationZ,bedsZ, c=casesLast, cmap=matplotlib.cm.cool)
+plt.title('beds vs population, colored by cases')
+plt.xlabel('County population Z')
+plt.ylabel('County hospital beds Z')
+
+
 
 plt.figure()
-plt.scatter(population[nonZeroMap],beds[nonZeroMap], c=casesLast[nonZeroMap], cmap=matplotlib.cm.cool)
-plt.title('beds vs population, colored by cases\nNo zero cases')
-plt.xlabel('County population')
-plt.ylabel('County hospital beds')
+plt.scatter(population,nonProf, c=casesLast, cmap=matplotlib.cm.cool)
+plt.title('Non profit hospitals per person vs cases')
+plt.xlabel('Non profit hospitals per person')
+plt.ylabel('Cases')
 
+plt.figure()
+plt.scatter(population,private, c=casesLast, cmap=matplotlib.cm.cool)
+plt.title('Private hospitals per person vs cases')
+plt.xlabel('Private hospitals per person')
+plt.ylabel('Cases')
+
+plt.figure()
+plt.scatter(population,governm, c=casesLast, cmap=matplotlib.cm.cool)
+plt.title('Government hospitals per person vs cases')
+plt.xlabel('Government hospitals per person')
+plt.ylabel('Cases')
+
+plt.show()
+
+print('\nDerive new variables basesd on a combination of existing variables')
 plt.figure()
 plt.scatter(10000*(beds/population),casesLast)
 plt.title('beds per 10000 people vs cases')
@@ -54,63 +68,9 @@ plt.xlabel('County beds per 10,000 people')
 plt.ylabel('Cases')
 
 plt.figure()
-plt.scatter(nonProf,casesLast)
-plt.title('Non profit hospitals vs cases')
-plt.xlabel('Non profit hospitals')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(private,casesLast)
-plt.title('Private hospitals vs cases')
-plt.xlabel('Private hospitals')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(governm,casesLast)
-plt.title('Government hospitals vs cases')
-plt.xlabel('Government hospitals')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(totalHosp,casesLast)
-plt.title('Hospitals vs cases')
-plt.xlabel('Hospitals')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(nonProf/population,casesLast)
-plt.title('Non profit hospitals per person vs cases')
-plt.xlabel('Non profit hospitals per person')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(private/population,casesLast)
-plt.title('Private hospitals per person vs cases')
-plt.xlabel('Private hospitals per person')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(governm/population,casesLast)
-plt.title('Government hospitals per person vs cases')
-plt.xlabel('Government hospitals per person')
-plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(totalHosp/population,casesLast)
+plt.scatter(population,totalHosp, c=casesLast, cmap=matplotlib.cm.cool)
 plt.title('Hospitals per person vs cases')
 plt.xlabel('Hospitals per person')
 plt.ylabel('Cases')
-
-plt.figure()
-plt.scatter(bedsZ,casesLast)
-plt.title('casesLast vs beds')
-plt.xlabel('County hospital beds Z')
-plt.ylabel('County cases')
-
-plt.figure()
-plt.scatter(populationZ,bedsZ, c=casesLast, cmap=matplotlib.cm.cool)
-plt.title('beds vs population, colored by cases')
-plt.xlabel('County population Z')
-plt.ylabel('County hospital beds Z')
 
 plt.show()
