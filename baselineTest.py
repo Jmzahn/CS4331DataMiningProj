8a94b8996e1b1c8cfcb3f07b0fb2f3359e0dd749
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-30 20:32:07 -0500

    redo

--- baselineTest.py
+++ baselineTest.py
@@ -1,53 +1,30 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
 import lib
 
-MTS = lib.loadMTS()
-#only grab this many rows : 307916
-MTS = MTS.head(307916)
-TRAIN_SPLIT = 77
-TRAIN_weeks = 11
-VAL_weeks = 3
-BATCH_SIZE = 3142
-BUFFER_SIZE = 10000
+features, targets, validate_features, validate_targets, dateRange = lib.loadNNData()
+
+targets = np.sum(targets, axis=1)
+validate_targets = np.sum(validate_targets, axis=1)
+index = np.arange(len(targets))
+slope, intercept, r_value, p_value, std_err = linregress(index,np.squeeze(targets))
+
+histIndex = np.arange(len(targets))
+futureIndex = np.arange(len(targets),len(targets)+len(validate_targets))
+
+predictions=[pred*slope+intercept for pred in futureIndex] 
+
+predIndex = np.arange(len(targets),len(targets)+len(predictions))
 
 
-
-#get date info
-dates = MTS['date'].values
-dateStart = np.datetime64(dates[0])
-dateEnd = np.datetime64(dates[-1])
-dateRange = np.arange(dateStart, dateEnd+np.timedelta64(1,'D'), dtype='datetime64[D]')
-days = len(dateRange)
-
-
-#grab feature and target data
-featuresNames = ['beds', 'helipads', 'nonProf', 'private', 'governm', 'tests', 'positive', 'negative']
-features = MTS[featuresNames].values.reshape((days, BATCH_SIZE, len(featuresNames)))
-targetsNames = ['cases']
-targets = MTS[targetsNames].values.reshape((days, BATCH_SIZE, len(targetsNames)))
-
-validate_features = features[TRAIN_SPLIT:].reshape(VAL_weeks*7, BATCH_SIZE, len(featuresNames))
-validate_targets = targets[TRAIN_SPLIT:].reshape(VAL_weeks*7, BATCH_SIZE, len(targetsNames))
-features = features[:TRAIN_SPLIT].reshape(TRAIN_weeks*7, BATCH_SIZE, len(featuresNames))
-targets = targets[:TRAIN_SPLIT].reshape(TRAIN_weeks*7, BATCH_SIZE, len(targetsNames))
-train_data_multi = tf.data.Dataset.from_tensor_slices((features, targets))
-val_data_multi = tf.data.Dataset.from_tensor_slices((validate_features, validate_targets))
-train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
-val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
-
-
-model = keras.models.Sequential()
-model.add(keras.layers.CuDNNLSTM(7, return_sequences=True, input_shape = features.shape[-2:], time_major=True))
-model.add(keras.layers.CuDNNLSTM(7, return_sequences=True))
-model.add(tf.keras.layers.Dense((targets.shape[-1])))
-model.summary()
-model.compile(optimizer='adam', loss='mean_absolute_error')
-
-
-multi_step_history = model.fit(train_data_multi, epochs=10, steps_per_epoch=77, validation_data=val_data_multi, validation_steps=21)
-
+plt.figure()
+plt.plot(histIndex, targets, 'b--', label='History')
+plt.plot(futureIndex, validate_targets, 'b:', label='Future')
+plt.plot(predIndex, predictions, 'r:', label='Prediction')
+plt.ylabel('Cases as proportion of Pop')
+plt.xlabel('Time')
+plt.legend(loc='upper left')
+plt.show()
